5e73c0c053f3ae13b14cb1580e7170ac860bd427
Gerry M <waltermccone88@gmail.com>
2021-05-04 19:23:30 -0500

    added a base variables to the service worker

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/manifest.webmanifest",
+    "/index.js",
+    "/style.css",
+    "/icons/icon-512x512.png",
+    "/icons/icon-192x192.png",
+]
